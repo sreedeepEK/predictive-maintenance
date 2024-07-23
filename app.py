b463bc744da76c9894bc4ab9c99bb09009ca69fe
Sreedeep EK <sreedeepek.95@gmail.com>
2024-07-23 13:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8080)
